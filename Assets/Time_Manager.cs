3ea5d35633cf5b729bdeb47466b79d4f7a0d4b19
OmniDev951 <ochsjax000@alpinesd.org>
2025-02-13 18:45:31 -0700

    Day Night cycle
    Made a basic day night cycle

---
+++ Assets/Time_Manager.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField] private Texture2D skyboxNight;
+    [SerializeField] private Texture2D skyboxSunrise;
+    [SerializeField] private Texture2D skyboxDay;
+    [SerializeField] private Texture2D skyboxSunset;
+    [SerializeField] private Gradient gradientNightToSunrise;
+    [SerializeField] private Gradient gradientSunriesToDay;
+    [SerializeField] private Gradient gradientDayToSunset;
+    [SerializeField] private Gradient gradientSunsetToNight;
+    [SerializeField] private Light globallight;
+
+    
+
+    private int minutes;
+    public int Minutes
+    {
+        get { return minutes; }
+        set
+        {
+            minutes = value;
+            OnMinutesChange(value);
+        }
+    }
+
+    private int hours;
+    public int Hours
+    {
+        get { return hours; }
+        set
+        {
+            hours = value;
+            OnHoursChange(value);
+        }
+    }
+
+    private int days;
+    public int Days
+    {
+        get { return days; }
+        set { days = value; }
+    }
+
+    private float tempSecond;
+
+    private void Update()
+    {
+        tempSecond += Time.deltaTime;
+        if (tempSecond >= 1)
+        {
+            Minutes += 1;
+            tempSecond = 0;
+        }
+    }
+
+    private void OnMinutesChange(int value)
+    {
+        globallight.transform.Rotate(Vector3.up, (1f / 1440f) * 360f, Space.World);
+        if (value >= 60)
+        {
+            Hours++;
+            minutes = 0;
+        }
+    }
+
+    private void OnHoursChange(int value)
+    {
+        if (value >= 24)
+        {
+            Hours = 0;
+            Days++;
+        }
+
+        if (value == 6)
+        {
+            StartCoroutine(LerpSkybox(skyboxNight, skyboxSunrise, 10f));
+            StartCoroutine(LerpLight(gradientNightToSunrise, 10f));
+        }
+        else if (value == 8)
+        {
+            StartCoroutine(LerpSkybox(skyboxSunrise, skyboxDay, 10f));
+            StartCoroutine(LerpLight(gradientSunriesToDay, 10f));
+        }
+        else if (value == 18)
+        {
+            StartCoroutine(LerpSkybox(skyboxDay, skyboxSunset, 10f));
+            StartCoroutine(LerpLight(gradientDayToSunset, 10f));
+        }
+        else if (value == 22)
+        {
+            StartCoroutine(LerpSkybox(skyboxSunset, skyboxNight, 10f));
+            StartCoroutine(LerpLight(gradientSunsetToNight, 10f));
+        }
+    }
+
+    private IEnumerator LerpSkybox(Texture2D a, Texture2D b, float time)
+    {
+        RenderSettings.skybox.SetTexture("_Texture1", a);
+        RenderSettings.skybox.SetTexture("_Texture2", b);
+        RenderSettings.skybox.SetFloat("_Blend", 0);
+
+        for (float i = 0; i < time; i += Time.deltaTime)
+        {
+            RenderSettings.skybox.SetFloat("_Blend", i / time);
+            yield return null;
+        }
+
+        RenderSettings.skybox.SetTexture("_Texture1", b);
+    }
+
+    private IEnumerator LerpLight(Gradient lightGradient, float time)
+    {
+        for (float i = 0; i < time; i += Time.deltaTime)
+        {
+            globallight.color = lightGradient.Evaluate(i / time); // Fixed typo
+            yield return null;
+        }
+    }
+}
+
