09ce444c39969adfd672063ad4f0c55a1f753d8b
OmniDev951 <ochsjax000@alpinesd.org>
2025-02-20 08:17:29 -0700

    Changes/Fixing scripts

--- Assets/TimeManager.cs
+++ Assets/TimeManager.cs
@@ -1,7 +1,7 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System; 
+using System.Collections; 
+using System.Collections.Generic; 
+using Unity.VisualScripting; using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
@@ -9,57 +9,93 @@ public class TimeManager : MonoBehaviour
     [SerializeField] private Texture2D skyboxSunrise;
     [SerializeField] private Texture2D skyboxDay;
     [SerializeField] private Texture2D skyboxSunset;
-    
+    [SerializeField] private Gradient gradientNightToSunrise;
+    [SerializeField] private Gradient gradientSunriseToDay;
+    [SerializeField] private Gradient gradientDayToSunset;
+    [SerializeField] private Gradient gradientSunsetToNight;
+    [SerializeField] private Light globalLight;
     private int minutes;
-    public int Minutes {get { return minutes; } set { minutes = value; OnMinutesChange(value); } }
-    
+
+    public int Minutes
+    {
+        get { return minutes; }
+        set
+        {
+            minutes = value;
+            OnMinutesChange(value);
+        }
+    }
+
     private int hours;
-    public int Hours {get { return hours; } set { hours = value; OnHoursChange(value); } }
-    
+
+    public int Hours
+    {
+        get { return hours; }
+        set
+        {
+            hours = value;
+            OnHoursChange(value);
+        }
+    }
+
     private int days;
-    public int Days {get {return days;} set {days = value;}}
-    private float tempSecond;
+
+    public int Days
+    {
+        get { return days; }
+        set { days = value; }
+    }
+
+    private float tempSeconds;
+
     public void Update()
     {
-        tempSecond += Time.deltaTime;
-        if (tempSecond >= 1)
+        tempSeconds += Time.deltaTime;
+        if (tempSeconds >= 1)
         {
-            minutes += 1;
-            tempSecond = 0;
+            Minutes += 1;
+            tempSeconds = 0;
+            
         }
     }
 
     private void OnMinutesChange(int value)
     {
-        if (value >= 60)
+        globalLight.transform.Rotate(Vector3.up, (1f / 1440f) * 360f, Space.World); // Rotate light for each minute 
+        if (value >= 60) // Changed 2 to 60 for realistic hour increments
         {
-            Hours += 1;
-            minutes = 0;
+            Hours++;
+            Minutes = 0; // Use setter to reset minutes 
         }
 
         if (Hours >= 24)
         {
             Hours = 0;
-            Days += 1;
+            Days++;
         }
     }
+
     private void OnHoursChange(int value)
     {
         if (value == 6)
         {
             StartCoroutine(LerpSkybox(skyboxNight, skyboxSunrise, 10f));
+            StartCoroutine(LerpLight(gradientNightToSunrise, 10f));
         }
         else if (value == 8)
         {
             StartCoroutine(LerpSkybox(skyboxSunrise, skyboxDay, 10f));
+            StartCoroutine(LerpLight(gradientSunriseToDay, 10f));
         }
         else if (value == 18)
         {
             StartCoroutine(LerpSkybox(skyboxDay, skyboxSunset, 10f));
+            StartCoroutine(LerpLight(gradientDayToSunset, 10f));
         }
         else if (value == 22)
         {
             StartCoroutine(LerpSkybox(skyboxSunset, skyboxNight, 10f));
+            StartCoroutine(LerpLight(gradientSunsetToNight, 10f));
         }
     }
 
@@ -70,9 +106,22 @@ public class TimeManager : MonoBehaviour
         RenderSettings.skybox.SetFloat("_Blend", 0);
         for (float i = 0; i < time; i += Time.deltaTime)
         {
-         RenderSettings.skybox.SetFloat("_Blend", i/time);
-         yield return null;
+            RenderSettings.skybox.SetFloat("_Blend", i / time);
+            yield return null;
         }
+
         RenderSettings.skybox.SetTexture("_Texture1", b);
+        RenderSettings.skybox.SetFloat("_Blend", 0); // Reset blend after transition
+    }
+
+    private IEnumerator LerpLight(Gradient LightGradient, float time)
+    {
+        for (float i = 0; i < time; i += Time.deltaTime)
+        {
+            globalLight.color = LightGradient.Evaluate(i / time);
+            yield return null;
+        }
     }
 }
+
+
