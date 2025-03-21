7aca45e6986f5048e2795756c1a1ed1a4157b8f3
Livilooo <wagner.olivia08@gmail.com>
2025-03-21 14:14:52 -0600

    Item Stuff yay

--- Assets/Scripts/ItemDatabase.cs
+++ Assets/Scripts/ItemDatabase.cs
@@ -12,6 +12,17 @@ public class ItemDatabase : MonoBehaviour
     void Start()
     {
         itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/Items.json"));
+        ConstructItemDatabase();
+        
+        Debug.Log(database[1].Title);
+    }
+
+    void ConstructItemDatabase()
+    {
+        for (int i = 0; i < itemData.Count; i++)
+        {
+            database.Add(new Item((int)itemData[i]["id"], itemData[i]["title"].ToString(), (int)itemData[i]["value"]));
+        }
     }
 }
 
@@ -20,9 +31,21 @@ public class Item
     public int ID { get; set; }
     public string Title { get; set; }
     public int Value { get; set; }
+    public string Type { get; set; }
+    public int Power { get; set; }
+    public int Defense { get; set; }
+    public int Chance { get; set; }
+    public int Health { get; set; }
+    public int Deals { get; set; }
+    public string Description { get; set; }
+    public bool Stackable { get; set; }
+    public int Rarity { get; set; }
+    public string Slug { get; set; }
 
     public Item(int id, string title, int value)
     {
         this.ID = id;
+        this.Title = title;
+        this.Value = value;
     }
 }
